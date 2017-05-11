67f7efc462a74ea6012c2b5dd9f051d1c8355f85
Joseph Papaluca <jpapaluc@gmail.com>
2017-05-11 12:33:52 -0400

    added about page, link to about page from index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'landing/index'
 
+  get 'landing/index'
   get 'landing/about'
+
+  root 'landing#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
