427fd6d06f14147700ff9b20a5a2b03478464606
Joseph Papaluca <jpapaluc@gmail.com>
2017-05-12 15:10:36 -0400

    added session routes, login form to layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
 
-  get 'sessions/new'
-
-  resources :users
+  root 'landing#index'
 
   get 'landing/index'
   get 'landing/about'
 
-  root 'landing#index'
+  get    '/login',              to: 'sessions#new'
+  post   '/login',              to: 'sessions#create'
+  delete '/logout',             to: 'sessions#destroy'
+
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
