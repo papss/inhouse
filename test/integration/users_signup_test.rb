9708a03a0c2307ad1f4bb489448d5a197aeb2842
Joseph Papaluca <jpapaluc@gmail.com>
2017-05-15 15:44:47 -0400

    added valid sign-up test

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -10,9 +10,21 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password:              "foo",
                                          password_confirmation: "bar" } }
       end
-      assert_template 'users/new'
-      # add tests to assert notice div message/css
-    end
+    assert_template 'users/new'
+    # add tests to assert notice div message/css
+  end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { user_name: "ExampleUser",
+                                         email_address: "user@valid.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
+      end
+    follow_redirect!
+    assert_template 'users/show'
+    # add tests to assert notice div message/css
   end
 
 end
