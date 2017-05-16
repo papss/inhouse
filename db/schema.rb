6c3f19d3402054d7eefa83cdbc8f86417b30a058
Joseph Papaluca <jpapaluc@gmail.com>
2017-05-16 14:44:06 -0400

    added remember digest

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170511213020) do
+ActiveRecord::Schema.define(version: 20170516184321) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20170511213020) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remeber_digest"
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
 
