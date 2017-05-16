c2cf51ad29c4d0a5e6e06d4bb728c54f28f73817
Joseph Papaluca <jpapaluc@gmail.com>
2017-05-16 12:19:27 -0400

    fixed login forms by changing form_with to form_for

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -24,6 +24,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       end
     follow_redirect!
     assert_template 'users/show'
+    assert is_logged_in? 
     # add tests to assert notice div message/css
   end
 
