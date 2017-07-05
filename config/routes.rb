b8492d2b848d25f4b9a5104f0a529357bc74849e
Joseph Papaluca <jpapaluc@gmail.com>
2017-07-05 16:46:43 -0400

    starting styling update

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,7 @@ Rails.application.routes.draw do
   get 'landing/index'
   get 'landing/about'
 
-  root 'landing#index'
+  root 'landing#about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
